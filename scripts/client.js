da2c73a661b7de6ecd104ae93909d6103d7031a8
Joe Schlachtenhaufen <JDSchlach@gmail.com>
2019-03-09 17:28:32 -0600

    click working object not displaying on dom

--- scripts/client.js
+++ scripts/client.js
@@ -1,21 +1,34 @@
 $( document ).ready( readyNow );
 console.log(`in JQuery`);
 let staff = [];
+//array
 
 function readyNow(){
-    $( '#addEmployeeButton' ).on( 'click', Submit )
-} 
+    $( '#addEmployeeButton' ).on( 'click', addEmployee )
+} // end ready now
 
-// function addItem(){
-//     console.log( 'in addItem' );
-//     let newItem = {
-//         fname: $( '#fNameIN' ).val(),
-//         lname: $( '#lNameIN' ).val(),
-//         id: $( '#idIn' ).val(),
-//         title: $( '#titleIn' ).val(),
-//         salary: $( '#salaryIn' ).val(),
-//     } // end newItem
-//     staff.push( newItem );
-//     console.log( 'Staff:', staff );
-//     displayStaff();
-// } // end addItem
+function addEmployee(){
+    console.log( 'in addStaff' );
+    let newEmployee = {
+        firstName: $( '#fNameIN' ).val(),
+        lastName: $( '#lNameIN' ).val(),
+        id: $( '#idIn' ).val(),
+        title: $( '#titleIn' ).val(),
+        salary: $( '#salaryIn' ).val(),
+    } // end new item
+    staff.push( newEmployee );
+    //push new employee to staff array
+    console.log( 'staff:', staff );
+    displayStaff();
+} // end add item
+
+ function displayStaff(){
+    console.log( 'in displayStaff' );
+    let el = $( '#staffOut' );
+    el.empty();
+     // loop staff
+  for( let item of staff ){
+     // for each item, create a new <li> in inventoryOut
+    el.append( `<li>${ newEmployee.firstName } & ${ newEmployee.lastName } & ${ newEmployee.id } & ${ newEmployee.title } & ${ newEmployee.salary }</li>` );
+    } // end loop
+ } // display staff
